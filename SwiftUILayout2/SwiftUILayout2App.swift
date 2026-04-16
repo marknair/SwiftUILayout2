c16e7e234d3b9c1e00dcf8ec92ce2b9c891ae8a2
Mark <marknair@me.com>
2026-04-16 08:55:45 -0500

    Initial Commit

---
+++ SwiftUILayout2/SwiftUILayout2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayout2App.swift
+//  SwiftUILayout2
+//
+//  Created by Mark Nair on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayout2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
